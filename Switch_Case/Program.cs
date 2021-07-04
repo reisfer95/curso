f82f8282ce4b61812b36533d2bd3b8ec687adfcb
Fernando Reis <reisfer95@gmail.com>
2021-07-04 11:13:55 -0300

    Projeto iniciado mostrando ola mundo

---
+++ Switch_Case/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+
+            Console.WriteLine(d1.Kind);
+
+
+            
+
+        }
+    }
+}
